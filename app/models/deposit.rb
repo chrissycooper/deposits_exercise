8266e3ac864208db72efa18e18aebff6eefd21fb
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-25 12:57:29 -0500

    Add psuedocode for amount over balance error handling

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,4 +2,5 @@ class Deposit < ApplicationRecord
   belongs_to :tradeline
 
   #validates deposit amount is less than tradeline balance
+  #validates :amount, comparison: {less_than: :outstanding_balance}
 end
