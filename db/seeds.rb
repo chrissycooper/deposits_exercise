dbedd3ca16bf2f42bd4d03866bf0bfd4d02a8756
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-24 21:10:21 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+home_loan = Tradeline.create(name: "Home Loan", amount: 196710.00)
+car_loan = Tradeline.create(name: "Car Loan", amount: 36685.00)
+personal_loan = Tradeline.create(name: "Personal Loan", amount: 4786.00)
+
