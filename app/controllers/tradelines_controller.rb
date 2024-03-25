da7fd17f2f65d5eacc243de50378be7d242df0a7
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-25 12:49:03 -0500

    Add ability to view outstanding balance in tradeline index route

--- app/controllers/tradelines_controller.rb
+++ app/controllers/tradelines_controller.rb
@@ -2,7 +2,23 @@ class TradelinesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
-    render json: Tradeline.all
+    @tradelines = Tradeline.all
+    @tradelines.each do |tradeline|
+      @tradeline = Tradeline.find(tradeline.id)
+      deposits_total = 0
+
+      @tradeline.deposits.each do |deposit|
+        deposits_total += deposit.amount
+      end
+
+      new_amount = @tradeline.amount - deposits_total
+
+      @tradeline.update(
+        outstanding_balance: new_amount
+      )
+    end
+
+    render json: @tradelines
   end
 
   def show
