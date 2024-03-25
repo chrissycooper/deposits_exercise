792bf6af43b16e656551ed9fc14c9abbfc4e3268
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-25 18:27:49 -0500

    Add some basic deposit model tests

--- spec/models/deposit_spec.rb
+++ spec/models/deposit_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Deposit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#initialize' do
+    it 'is an instance of deposit' do
+      deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+      expect(deposit).to be_a Deposit
+    end
+
+    it 'has a date' do
+      deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+      expect(deposit.date_of_deposit).to eq '05/05/2024'
+    end
+  end
 end
