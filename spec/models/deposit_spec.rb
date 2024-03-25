9269955dd3913e0b61e219c47f40ae592f92b8d2
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-25 18:39:24 -0500

    Refactor tests

--- spec/models/deposit_spec.rb
+++ spec/models/deposit_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Deposit, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
   describe '#initialize' do
+    before(:each) do
+      @deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+    end
+
     it 'is an instance of deposit' do
-      deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
-      expect(deposit).to be_a Deposit
+      expect(@deposit).to be_a Deposit
     end
 
     it 'has a date' do
-      deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
-      expect(deposit.date_of_deposit).to eq '05/05/2024'
+      expect(@deposit.date_of_deposit).to eq '05/05/2024'
     end
 
     it 'has an amount' do
-      deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
-      expect(deposit.amount).to eq 1000.00
+      expect(@deposit.amount).to eq 1000.00
     end
   end
 end
