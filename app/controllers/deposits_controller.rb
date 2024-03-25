a26faa587cd282cd6d83057d97daae76fa3ef271
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-25 11:10:16 -0500

    Add ability to create a new deposit

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,2 +1,11 @@
 class DepositsController < ApplicationController
+  def create
+    @tradeline = Tradeline.find(params[:tradeline_id])
+    @deposit = @tradeline.deposits.create(
+      date_of_deposit: params[:date_of_deposit],
+      amount: params[:amount]
+    )
+
+    render json: @deposit
+  end
 end
