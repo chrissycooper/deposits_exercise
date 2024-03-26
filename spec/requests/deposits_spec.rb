1ae509f1d0069a9230e10c3a316cb71ee06ab6d4
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-26 14:50:00 -0500

    Add post request test for deposits

--- spec/requests/deposits_spec.rb
+++ spec/requests/deposits_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Deposits", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /create" do
+    let(:tradeline) { FactoryBot.create :tradeline }
+    
+    before do
+      @deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+      post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit.date_of_deposit, amount: @deposit.amount}
+      # get "/tradelines/#{tradeline.id}", headers: headers
+    end
+
+    it "responds with a success status" do
+      expect(response).to be_ok
+    end
   end
 end
