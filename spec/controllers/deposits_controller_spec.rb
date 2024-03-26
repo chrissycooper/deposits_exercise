bbe21ceb5e286422e6e2e1a1a78e7670a2e8a74a
Chrissy Cooper <cooper.christine.r@gmail.com>
2024-03-26 15:03:50 -0500

    Update deposits requests tests

--- spec/controllers/deposits_controller_spec.rb
+++ spec/controllers/deposits_controller_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe DepositsController, type: :controller do
-  xdescribe '#index' do
-  let(:tradeline) { FactoryBot.create :tradeline }
-  let(:deposit) { FactoryBot.create :deposit }
-    it 'responds with a 200' do
-      get :index, params: { tradeline_id: tradeline.id }
+  # xdescribe '#index' do
+  # let(:tradeline) { FactoryBot.create :tradeline }
+  # let(:deposit) { FactoryBot.create :deposit }
+  #   it 'responds with a 200' do
+  #     get :index, params: { tradeline_id: tradeline.id }
 
-      expect(response).to have_http_status(:ok)
-    end
-  end
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
 
-  xdescribe '#show' do
-    let(:tradeline) { FactoryBot.create :tradeline }
-    let(:deposit) { FactoryBot.create :deposit }
+  # xdescribe '#show' do
+  #   let(:tradeline) { FactoryBot.create :tradeline }
+  #   let(:deposit) { FactoryBot.create :deposit }
 
-    it 'responds with a 200' do
-      get :show, params: { tradeline_id: tradeline.id, id: deposit.id }
-      expect(response).to have_http_status(:ok)
-    end
+  #   it 'responds with a 200' do
+  #     get :show, params: { tradeline_id: tradeline.id, id: deposit.id }
+  #     expect(response).to have_http_status(:ok)
+  #   end
 
-    context 'if the loan is not found' do
-      it 'responds with a 404' do
-        get :show, params: { id: 1000 }
-        expect(response).to have_http_status(:not_found)
-      end
-    end
-  end
+  #   context 'if the loan is not found' do
+  #     it 'responds with a 404' do
+  #       get :show, params: { id: 1000 }
+  #       expect(response).to have_http_status(:not_found)
+  #     end
+  #   end
+  # end
 end
--- spec/requests/deposits_spec.rb
+++ spec/requests/deposits_spec.rb
@@ -7,11 +7,42 @@ RSpec.describe "Deposits", type: :request do
     before do
       @deposit = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
       post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit.date_of_deposit, amount: @deposit.amount}
-      # get "/tradelines/#{tradeline.id}", headers: headers
     end
 
     it "responds with a success status" do
       expect(response).to be_ok
+    end
+  end
+
+  describe "GET /index" do
+    let(:tradeline) { FactoryBot.create :tradeline }
+    
+    before do
+      @deposit1 = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+      @deposit2 = Deposit.new({date_of_deposit: '05/07/2024', amount: 1000.00})
+      post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit1.date_of_deposit, amount: @deposit1.amount}
+      post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit2.date_of_deposit, amount: @deposit2.amount}
+      get "/tradelines/#{tradeline.id}/deposits"
+    end
+
+    it "responds with a success status" do
+      expect(response).to be_ok
+    end
+  end
+
+  describe "GET /show" do
+    let(:tradeline) { FactoryBot.create :tradeline }
+    
+    before do
+      @deposit1 = Deposit.new({date_of_deposit: '05/05/2024', amount: 1000.00})
+      @deposit2 = Deposit.new({date_of_deposit: '05/07/2024', amount: 1000.00})
+      post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit1.date_of_deposit, amount: @deposit1.amount}
+      post "/tradelines/#{tradeline.id}/deposits", params: {date_of_deposit: @deposit2.date_of_deposit, amount: @deposit2.amount}
+      get "/tradelines/#{tradeline.id}/deposits/3"
+    end
+
+    it "responds with a failure status" do
+      expect(response).to have_http_status(404)
     end
   end
 end
